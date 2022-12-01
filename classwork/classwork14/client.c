6251a65a1c068ef39179bd693856ec8fdf8fc9e2
mariadmdc <89882682+mariadmdc@users.noreply.github.com>
2022-11-30 17:10:31 -0800

    Add files via upload

---
+++ classwork/classwork14/client.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PORT 8080
+
+int main( int argc, char const *argv[] ) {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if( (sock = socket( AF_INET, SOCK_STREAM, 0 )) < 0 ) {
+        printf( "\n Socket creation error \n" );
+        return -1;
+    }
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    if( inet_pton( AF_INET, "127.0.0.1", &serv_addr.sin_addr )<= 0 ) {
+        printf( "\nInvalid address or Address not supported \n" );
+        return -1;
+    }
+    if( connect( sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0 ) {
+        printf( "\nConnection Failed \n" );
+        return -1;
+    }
+    while(1) {
+        send( sock, hello, strlen(hello), 0 );
+        printf( "   client says: Hello message sent from client\n" );
+        valread = read( sock, buffer, 1024 );
+        printf( "   client says: server sent: %s\n", buffer );
+        printf("Do you want to hear me say hello again? (Y/N)");
+        fgets(buffer, 20, stdin);
+        if (strcmp(buffer, "N") == 0) {
+            break;
+        }
+    }
+}
